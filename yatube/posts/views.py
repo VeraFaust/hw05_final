f2697f6dcb0f8ecf092a847e0e979b11b55c2908
Вера Фауст <faust.vera@yandex.ru>
2022-12-26 14:08:54 +0300

    final_6

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -140,7 +140,7 @@ def follow_index(request):
 @login_required
 def profile_follow(request, username):
     author = get_object_or_404(User, username=username)
-    follow = Follow.objects.filter(
+    Follow.objects.filter(
         user=request.user,
         author=author
     )
